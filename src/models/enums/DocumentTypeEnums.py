bdd61b125512cf7326dc13282dbe7a21719dc79c
Diab_Saeed <diabsaeed7@gmail.com>
2026-06-21 18:49:36 +0300

    adding DocumentTypeEnums

---
+++ src/models/enums/DocumentTypeEnums.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+class DocumentTypeEnums(Enum):
+    PDF = "pdf"
+    WORD = "word"
+    EXCEL = "excel"
+    POWERPOINT = "powerpoint"
+    TEXT = "text"
--- src/models/enums/ResponseEnum.py
+++ src/models/enums/ResponseEnum.py
@@ -7,3 +7,4 @@ class ResponseEnums(Enum):
     FILE_UPLOADED_FIELD = "File Field"
     PROCESS_STARTED_SUCCE = "process_started_successfully"
     ProjectNotfoundERROR = "Project Not Found"
+    IndexingFailedERROR = "Indexing Failed"
